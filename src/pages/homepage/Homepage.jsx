cd7110cb6a31f09a7539aa7cbe87e6df15c88a68
Emil Hölvold <emilholvold@Emils-Air.lan>
2021-08-14 16:31:53 +0200

    added a startpage

---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Homepage.scss'
+import Directory from '../../components/directory/Directory'
+
+const homepage = () => {
+  return( 
+    <div className="homepage">    
+      <Directory />
+    </div>
+  )}
+
+export default homepage
