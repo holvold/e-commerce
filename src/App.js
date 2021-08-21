b2b779f72f9bd50bca134074954f3e50d8ebb70e
Emil Hölvold <emilholvold@Emils-Air.lan>
2021-08-21 22:03:14 +0200

    Added routing so additional pages are valid aswell

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
+
 import './App.css';
 import Homepage from './pages/homepage/Homepage';
+
+function HatsPage() {
+  return(  
+    <div>
+      <h1>HatsPage</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <Homepage/>
+      <Switch>    
+        <Route exact={true} path='/' component={Homepage} />
+        <Route exact={true} path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
